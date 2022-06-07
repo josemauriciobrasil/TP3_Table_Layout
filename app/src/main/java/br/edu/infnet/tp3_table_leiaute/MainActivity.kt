2d3f34ba822df311ecda489b6fe0bb5a9c190a77
Jose Mauricio <josemauriciobrasil@gmail.com>
2022-06-06 23:17:49 -0300

    primeiro commit

---
+++ app/src/main/java/br/edu/infnet/tp3_table_leiaute/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.infnet.tp3_table_leiaute
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
